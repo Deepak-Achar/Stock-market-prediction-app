e42508081864943b10d8a43e144bcd391facdc51
Deepak-Achar <741236985deepak@gmail.com>
2024-12-09 20:43:58 +0530

    first commit

---
+++ stocks.py
@@ -0,0 +1,195 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+from ta.volatility import BollingerBands
+from ta.trend import MACD, EMAIndicator, SMAIndicator
+from ta.momentum import RSIIndicator
+import datetime
+from datetime import date
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KNeighborsRegressor
+from xgboost import XGBRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.metrics import r2_score, mean_absolute_error
+from stocknews import StockNews
+from alpha_vantage.fundamentaldata import FundamentalData
+
+st.title('Stock Price Predictions')
+# st.sidebar.info('Welcome to the Stock Price Prediction App. Choose your options below')
+# st.sidebar.info("Created and designed by [Fayaz Patawari](https://www.linkedin.com/in/g-p-manoj-kumar/)")
+
+# Add this function
+def get_stock_news(ticker):
+    with st.spinner("Fetching News..."):
+        st.header(f'News of {ticker}')
+        sn = StockNews(ticker, save_news=False)
+        df_news = sn.read_rss()
+        for i in range(min(len(df_news), 10)):
+            st.subheader(f'News {i+1}')
+            st.write("Published Date:", df_news['published'][i])
+            st.write("Title:", df_news['title'][i])
+            st.write("Summary:", df_news['summary'][i])
+            st.write(f'Title Sentiment: {df_news["sentiment_title"][i]}')
+            st.write(f'News Sentiment: {df_news["sentiment_summary"][i]}')
+            st.write("------------------------------------------------------------------")
+
+# Add this function
+def get_financial_statements(ticker):
+    key = 'OW1639L63B5UCYYL'
+    fd = FundamentalData(key, output_format='pandas')
+    st.subheader('Balance Sheet')
+    balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
+    bs = balance_sheet.T[2:]
+    bs.columns = list(balance_sheet.T.iloc[0])
+    st.write(bs)
+    st.subheader('Income Statement')
+    income_statement = fd.get_income_statement_annual(ticker)[0]
+    is1 = income_statement.T[2:]
+    is1.columns = list(income_statement.T.iloc[0])
+    st.write(is1)
+    st.subheader('Cash Flow Statement')
+    cash_flow = fd.get_cash_flow_annual(ticker)[0]
+    cf = cash_flow.T[2:]
+    cf.columns = list(cash_flow.T.iloc[0])
+    st.write(cf)
+
+def main():
+    option = st.sidebar.selectbox('Make a choice', ['Visualize','Recent Data', 'Predict', 'Stock News', 'Financial Statements'])
+    if option == 'Visualize':
+        tech_indicators()
+    elif option == 'Recent Data':
+        dataframe()
+    elif option == 'Predict':
+        predict()
+   
+    elif option == 'Stock News':
+        ticker = st.text_input("Enter Stock Ticker:")
+        if ticker:
+            get_stock_news(ticker)
+    elif option == 'Financial Statements':
+        ticker = st.text_input("Enter Stock Ticker:")
+        if ticker:
+            get_financial_statements(ticker)
+
+@st.cache_resource
+def download_data(op, start_date, end_date):
+    df = yf.download(op, start=start_date, end=end_date, progress=False)
+    return df
+
+option = st.sidebar.text_input('Enter a Stock Symbol', value='SPY')
+option = option.upper()
+today = datetime.date.today()
+duration = st.sidebar.number_input('Enter the duration', value=3000)
+before = today - datetime.timedelta(days=duration)
+start_date = st.sidebar.date_input('Start Date', value=before)
+end_date = st.sidebar.date_input('End date', today)
+if st.sidebar.button('Send'):
+    if start_date < end_date:
+        st.sidebar.success('Start date: `%s`\n\nEnd date: `%s`' %(start_date, end_date))
+        download_data(option, start_date, end_date)
+    else:
+        st.sidebar.error('Error: End date must fall after start date')
+
+data = download_data(option, start_date, end_date)
+scaler = StandardScaler()
+
+def tech_indicators():
+    st.header('Technical Indicators')
+    option = st.radio('Choose a Technical Indicator to Visualize', ['Close', 'BB', 'MACD', 'RSI', 'SMA', 'EMA'])
+
+    # Bollinger bands
+    bb_indicator = BollingerBands(data.Close)
+    bb = data
+    bb['bb_h'] = bb_indicator.bollinger_hband()
+    bb['bb_l'] = bb_indicator.bollinger_lband()
+    # Creating a new dataframe
+    bb = bb[['Close', 'bb_h', 'bb_l']]
+    # MACD
+    macd = MACD(data.Close).macd()
+    # RSI
+    rsi = RSIIndicator(data.Close).rsi()
+    # SMA
+    sma = SMAIndicator(data.Close, window=14).sma_indicator()
+    # EMA
+    ema = EMAIndicator(data.Close).ema_indicator()
+
+    if option == 'Close':
+        st.write('Close Price')
+        st.line_chart(data.Close)
+    elif option == 'BB':
+        st.write('BollingerBands')
+        st.line_chart(bb)
+    elif option == 'MACD':
+        st.write('Moving Average Convergence Divergence')
+        st.line_chart(macd)
+    elif option == 'RSI':
+        st.write('Relative Strength Indicator')
+        st.line_chart(rsi)
+    elif option == 'SMA':
+        st.write('Simple Moving Average')
+        st.line_chart(sma)
+    else:
+        st.write('Expoenetial Moving Average')
+        st.line_chart(ema)
+
+def dataframe():
+    st.header('Recent Data')
+    st.dataframe(data.tail(20))
+
+def predict():
+    model = st.radio('Choose a model', ['LinearRegression', 'RandomForestRegressor', 'ExtraTreesRegressor', 'KNeighborsRegressor', 'XGBoostRegressor'])
+    num = st.number_input('How many days forecast?', value=5)
+    num = int(num)
+    if st.button('Predict'):
+        if model == 'LinearRegression':
+            engine = LinearRegression()
+            model_engine(engine, num)
+        elif model == 'RandomForestRegressor':
+            engine = RandomForestRegressor()
+            model_engine(engine, num)
+        elif model == 'ExtraTreesRegressor':
+            engine = ExtraTreesRegressor()
+            model_engine(engine, num)
+        elif model == 'KNeighborsRegressor':
+            engine = KNeighborsRegressor()
+            model_engine(engine, num)
+        else:
+            engine = XGBRegressor()
+            model_engine(engine, num)
+
+def model_engine(model, num):
+    # getting only the closing price
+    df = data[['Close']]
+    # shifting the closing price based on number of days forecast
+    df['preds'] = data.Close.shift(-num)
+    # scaling the data
+    x = df.drop(['preds'], axis=1).values
+    x = scaler.fit_transform(x)
+    # storing the last num_days data
+    x_forecast = x[-num:]
+    # selecting the required values for training
+    x = x[:-num]
+    # getting the preds column
+    y = df.preds.values
+    # selecting the required values for training
+    y = y[:-num]
+
+    #spliting the data
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.2, random_state=7)
+    # training the model
+    model.fit(x_train, y_train)
+    preds = model.predict(x_test)
+    st.text(f'r2_score: {r2_score(y_test, preds)} \
+            \nMAE: {mean_absolute_error(y_test, preds)}')
+    # predicting stock price based on the number of days
+    forecast_pred = model.predict(x_forecast)
+    day = 1
+    for i in forecast_pred:
+        st.text(f'Day {day}: {i}')
+        day += 1
+
+if __name__ == '__main__':
+    main()
